39070ec4d19523e96c7ec7e16f7419c93a6a3804
KM Manish <156883335+Manish50518@users.noreply.github.com>
2024-10-29 00:10:18 +0530

    adding the envs

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ojhyoprgrmmnpiaavsiq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qaHlvcHJncm1tbnBpYWF2c2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0NjcyMzYsImV4cCI6MjA0MjA0MzIzNn0.lK6ZuAa1ief6QdDd6aatGjLGilQEI-WKyXeBHPVI714";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
