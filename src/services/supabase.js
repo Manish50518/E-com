712a3ee2e34b6a255fa7d38c27904f5766fef82c
KM Manish <156883335+Manish50518@users.noreply.github.com>
2024-10-29 10:59:17 +0530

    some modification

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://ojhyoprgrmmnpiaavsiq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qaHlvcHJncm1tbnBpYWF2c2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0NjcyMzYsImV4cCI6MjA0MjA0MzIzNn0.lK6ZuAa1ief6QdDd6aatGjLGilQEI-WKyXeBHPVI714";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
